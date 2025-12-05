f385a2305c0d960a91e6c754b60a0044f8d12277
Broderick Beaman <broderick.beaman.dev@gmail.com>
2025-12-05 08:30:47 -0400

    Dec05 solutions

---
+++ src/dec06/Dec06.java
@@ -0,0 +1,20 @@
+package dec06;
+
+import framework.AOCParent;
+
+public class Dec06 extends AOCParent {
+    @Override
+    public void loadInput() {
+
+    }
+
+    @Override
+    public void part1() {
+
+    }
+
+    @Override
+    public void part2() {
+
+    }
+}
---
+++ src/dec06/InputLoader.java
@@ -0,0 +1,6 @@
+package dec06;
+
+import framework.InputLoaderParent;
+
+public class InputLoader extends InputLoaderParent {
+}
