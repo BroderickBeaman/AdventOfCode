f9aec350871713ebfaf4b1c5172fb9b6ba1def05
Broderick Beaman <broderick.beaman.dev@gmail.com>
2025-12-10 19:19:58 -0400

    Prep for Dec11

---
+++ src/dec11/Dec11.java
@@ -0,0 +1,20 @@
+package dec11;
+
+import framework.AOCParent;
+
+public class Dec11 extends AOCParent {
+    @Override
+    public void loadInput() {
+
+    }
+
+    @Override
+    public void part1() {
+
+    }
+
+    @Override
+    public void part2() {
+
+    }
+}
