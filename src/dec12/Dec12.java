b998a25b9d7a7bdca392fe8f280363809df994e5
Broderick Beaman <broderick.beaman.dev@gmail.com>
2025-12-12 10:50:06 -0400

    Dec12 solutions

--- src/dec12/Dec12.java
+++ src/dec12/Dec12.java
@@ -2,15 +2,21 @@ package dec12;
 
 import framework.AOCParent;
 
+import java.util.List;
+
 public class Dec12 extends AOCParent {
+
+    private List<PresentSlot> presentSlots;
+
     @Override
     public void loadInput() {
-
+        presentSlots = InputLoader.loadPresents();
     }
 
     @Override
     public void part1() {
-
+        long numFits = presentSlots.stream().filter(PresentSlot::fits).count();
+        printSolution(numFits);
     }
 
     @Override
---
+++ src/dec12/PresentSlot.java
@@ -0,0 +1,7 @@
+package dec12;
+
+public record PresentSlot(Long rows, Long cols, Long numPresents) {
+    public boolean fits() {
+        return numPresents <= (rows / 3) * (cols / 3);
+    }
+}
